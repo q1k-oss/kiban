575fcd32c2f75ae7b0721b0104f8a7e867d944f8
anuj-karan-q1k <anuj@q1k.ai>
2025-11-12 13:12:30 +0530

    fix: added border-bottom to toast and apply distinct colors for success, warning, info, and error states

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -1,32 +1,41 @@
-"use client"
+"use client";
 
-import { useTheme } from "next-themes"
-import * as React from "react"
-import { Toaster as Sonner } from "sonner"
+import { useTheme } from "next-themes";
+import * as React from "react";
+import { Toaster as Sonner } from "sonner";
 
-type ToasterProps = React.ComponentProps<typeof Sonner>
+type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  const { theme = "system" } = useTheme();
 
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
-      className="toaster group"
+      className="toaster group "
       toastOptions={{
         classNames: {
           toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg !border-0 !border-b-4",
           description: "group-[.toast]:text-muted-foreground",
           actionButton:
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
           cancelButton:
             "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+          success: "!border-success ",
+
+          error: " !border-error-border-2 ",
+
+          warning: " !border-status-text-paused ",
+
+          info:"!border-toast-information ",
         },
+        duration: 5000,
       }}
+      position="top-right"
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster }
+export { Toaster };
