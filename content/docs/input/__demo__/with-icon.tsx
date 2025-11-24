f74b4ce6b3e17f264f33e2e834ac993bb71ce362
anuj-karan-q1k <anuj@q1k.ai>
2025-11-24 16:18:54 +0530

    fix(ui) - added with-icon variant of input

---
+++ content/docs/input/__demo__/with-icon.tsx
@@ -0,0 +1,28 @@
+import { BadgeAlert, BadgeCheck } from "lucide-react";
+import React from "react";
+
+import { Input } from "@happect/ethereal-ui";
+
+export default () => {
+  const temp = (
+    <div className="flex gap-1">
+      <BadgeCheck size={20} className="text-status-text-published" />
+      <p className="text-sm text-primary-text font-light">Verified</p>
+    </div>
+  );
+
+  const temp2 = (
+    <div className="flex gap-1">
+      <BadgeAlert size={20} className="text-status-text-paused" />
+      <p className="text-sm text-primary-text font-light">Verify</p>
+    </div>
+  );
+
+  return (
+    <div className="flex flex-col gap-4">
+      <Input type="email" defaultValue="demoq1k.ai" readOnly icon={temp} />
+
+      <Input type="email" defaultValue="demoq1k.ai" readOnly icon={temp2} />
+    </div>
+  );
+};
