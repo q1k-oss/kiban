523d1f7d409a9223418402c942b412fd1fb71b50
anuj-karan-q1k <anuj@q1k.ai>
2025-11-29 22:36:50 +0530

    Fix: Remove the pointer-events-none class so the icon can receive clicks and the onClick action works properly.

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -13,8 +13,8 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           type={type}
           className={cn(
             "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-            icon && "pr-10",
-            className
+            className,
+            icon && "pr-22",
           )}
           ref={ref}
           {...props}
@@ -22,7 +22,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 
         {/* Only show icon if children exist */}
         {icon && (
-          <div className="absolute inset-y-0 right-3 flex items-center pointer-events-none">
+          <div className="absolute inset-y-0 right-3 flex items-center">
             {icon}
           </div>
         )}
