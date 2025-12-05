3ef6de4a650c392a3c71dfa3a508698fb5cd6ff7
anuj-karan-q1k <anuj@q1k.ai>
2025-12-05 13:59:14 +0530

    feat(stepper): executing of next() and previous() method of stepper

--- content/docs/stepper/__demo__/basic.tsx
+++ content/docs/stepper/__demo__/basic.tsx
@@ -1,86 +1,24 @@
 "use client";
-import { useState } from "react";
+import { useRef } from "react";
 
-import { Button, Stepper } from "@happect/ethereal-ui";
-
-const STEP_STATUS = {
-  PENDING: "pending",
-  ON_GOING: "on_going",
-  COMPLETED: "completed",
-};
+import { Button, IStepItem, Stepper } from "@happect/ethereal-ui";
 
 export default function StepperDemo() {
-  const [steps, setSteps] = useState([
-    { LABEL: "Verify Current Number", STATUS: STEP_STATUS.ON_GOING }, // FIX 🔥
-    { LABEL: "Change Phone Number", STATUS: STEP_STATUS.PENDING },
-    { LABEL: "Verify New Number", STATUS: STEP_STATUS.PENDING },
-  ]);
+  const stepperRef = useRef<any>(null);
 
-  let animLock = false; // keeps animation in sync
-
-  const nextStep = () => {
-    if (animLock) return;
-    animLock = true;
-
-    const idx = steps.findIndex((s) => s.STATUS === STEP_STATUS.ON_GOING);
-
-    if (idx === -1 || idx === steps.length) return;
-
-    // mark active as completed
-    setSteps((prev) => {
-      const copy = [...prev];
-      copy[idx].STATUS = STEP_STATUS.COMPLETED;
-      return copy;
-    });
-    if (idx === steps.length - 1) return;
-    // after animation → activate next
-    setTimeout(() => {
-      setSteps((prev) => {
-        const copy = [...prev];
-        copy[idx + 1].STATUS = STEP_STATUS.ON_GOING;
-        return copy;
-      });
-      animLock = false;
-    }, 1000);
-  };
-
-  const prevStep = () => {
-    if (animLock) return;
-    animLock = true;
-
-    const idx = steps.findIndex((s) => s.STATUS === STEP_STATUS.ON_GOING);
-    if (idx <= 0) return;
-
-    // revert current to pending
-    setSteps((prev) => {
-      const copy = [...prev];
-      copy[idx].STATUS = STEP_STATUS.PENDING;
-      return copy;
-    });
-
-    // after animation → previous active
-    setTimeout(() => {
-      setSteps((prev) => {
-        const copy = [...prev];
-        copy[idx - 1].STATUS = STEP_STATUS.ON_GOING;
-
-        // clean next steps if user goes backward
-        for (let i = idx; i < copy.length; i++)
-          copy[i].STATUS = STEP_STATUS.PENDING;
-
-        return copy;
-      });
-      animLock = false;
-    }, 600);
-  };
+  const steps: IStepItem[] = [
+    { LABEL: "Verify Current Number", STATUS: 'on_going' },
+    { LABEL: "Change Phone Number", STATUS: "pending" },
+    { LABEL: "Verify New Number", STATUS: "pending" },
+  ];
 
   return (
     <div className="flex flex-col justify-center items-center gap-12">
-      <Stepper steps={steps} />
+      <Stepper ref={stepperRef} steps={steps} />
 
       <div className="flex items-center gap-4">
-        <Button onClick={prevStep}>Previous</Button>
-        <Button onClick={nextStep}>Next</Button>
+        <Button onClick={() => stepperRef.current.prev()}>Previous</Button>
+        <Button onClick={() => stepperRef.current.next()}>Next</Button>
       </div>
     </div>
   );
