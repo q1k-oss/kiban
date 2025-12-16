9f52a0447fb7a2b7e0fdca2578810938a555b538
Himanshu Shekhar <contact@hshekhar.com>
2025-12-16 14:34:28 +0530

    dist update

---
+++ dist/esm/icons/FilterIcon.d.ts
@@ -0,0 +1,6 @@
+interface FilterIconProps {
+    className?: string;
+    size?: number;
+}
+export declare const FilterIcon: ({ className, size }: FilterIconProps) => import("react/jsx-runtime").JSX.Element;
+export {};
