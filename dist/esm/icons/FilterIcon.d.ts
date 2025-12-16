47e40c140502fbe06d8ab3b59b4d25a9e5382d9c
Anuj Karan <anuj@q1k.ai>
2025-12-16 14:59:33 +0530

    Revert "Feature/add filter icon and css variables"

--- dist/esm/icons/FilterIcon.d.ts
+++
@@ -1,6 +0,0 @@
-interface FilterIconProps {
-    className?: string;
-    size?: number;
-}
-export declare const FilterIcon: ({ className, size }: FilterIconProps) => import("react/jsx-runtime").JSX.Element;
-export {};
