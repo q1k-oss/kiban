2b489936abe657db558f975957959d32bc7e8b9d
anuj-karan-q1k <anuj@q1k.ai>
2025-12-16 16:14:04 +0530

    feat: conflict resolved

---
+++ dist/esm/icons/CreateAgentIcon.d.ts
@@ -0,0 +1,6 @@
+interface CreateAgentIconProps {
+    className?: string;
+    size?: number;
+}
+export default function CreateAgentIcon({ className, size, }: CreateAgentIconProps): import("react/jsx-runtime").JSX.Element;
+export {};
