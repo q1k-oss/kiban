4388bd0199f0376fd36a88dffa619fb53b46d483
anuj-karan-q1k <anuj@q1k.ai>
2025-12-18 23:34:21 +0530

    feat: dist dump

--- dist/esm/icons/FilterIcon.d.ts
+++ dist/esm/icons/FilterIcon.d.ts
@@ -1,6 +1,7 @@
 interface FilterIconProps {
     className?: string;
     size?: number;
+    strokeWidth?: number;
 }
-export declare const FilterIcon: ({ className, size }: FilterIconProps) => import("react/jsx-runtime").JSX.Element;
+export declare const FilterIcon: ({ className, strokeWidth, size, }: FilterIconProps) => import("react/jsx-runtime").JSX.Element;
 export {};
