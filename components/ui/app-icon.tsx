b36fb3e916c64d577604ab6f5a3f7679b212f161
anuj-karan-q1k <anuj@q1k.ai>
2025-12-19 16:59:15 +0530

    feat: app-icon implemented

---
+++ components/ui/app-icon.tsx
@@ -0,0 +1,38 @@
+import { DynamicIcon, type IconName } from "lucide-react/dynamic";
+import React from "react";
+
+import { CustomIcons, type CustomIconName } from "../icons";
+
+type AppIconName = CustomIconName | IconName;
+type IconNameWithSuggestions = AppIconName | (string & {});
+
+interface AppIconProps {
+  iconName: IconNameWithSuggestions;
+  size?: number;
+  strokeWidth?: number;
+  className?: string;
+  fallbackIcon?: IconName;
+}
+
+export const AppIcon: React.FC<AppIconProps> = ({
+  iconName,
+  size = 20,
+  strokeWidth = 1.5,
+  className,
+}) => {
+  if (iconName in CustomIcons) {
+    const CustomIcon = CustomIcons[iconName as CustomIconName];
+    return (
+      <CustomIcon size={size} strokeWidth={strokeWidth} className={className} />
+    );
+  }
+
+  return (
+    <DynamicIcon
+      name={iconName as IconName}
+      size={size}
+      strokeWidth={strokeWidth}
+      className={className}
+    />
+  );
+};
---
+++ content/docs/app-icon/__demo__/basic.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { AppIcon } from "@happect/ethereal-ui/ui/app-icon";
+
+export default function basic() {
+  return (
+    <div className="flex items-center justify-between gap-2 w-full">
+      <AppIcon
+        iconName={"layout-dashboard"}
+        size={40}
+        className="text-muted-foreground"
+      />
+      <AppIcon
+        iconName={"FolderQ1kIcon"}
+        size={40}
+        className="text-muted-foreground"
+      />
+      <AppIcon
+        iconName={"TokenIcon"}
+        size={40}
+        className="text-muted-foreground"
+      />
+      <AppIcon
+        iconName={"rocket"}
+        size={40}
+        className="text-muted-foreground"
+      />
+      <AppIcon iconName={"bot"} size={40} className="text-muted-foreground" />
+    </div>
+  );
+}
