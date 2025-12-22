4ab03caef68d9f5d8d43b4061bd699de4bc4dc44
anuj-karan-q1k <anuj@q1k.ai>
2025-12-22 16:57:09 +0530

    fix: convert customIcon into readable key:pair format

--- components/icons/index.tsx
+++ components/icons/index.tsx
@@ -1,21 +1,29 @@
 import { ArrowUpIcon } from "./ArrowUpIcon";
 import { CheckIcon } from "./CheckIcon";
 import { CreateAgentIcon } from "./CreateAgentIcon";
+import { FileCSVIcon } from "./FileCSVIcon";
+import { FileDOCIcon } from "./FileDOCIcon";
+import { FilePDFIcon } from "./FilePDFIcon";
 import { FilterIcon } from "./FilterIcon";
 import { FolderQ1kIcon } from "./FolderQ1kIcon";
+import { GoogleDriveIcon } from "./GoogleDriveIcon";
 import { PaperClipIcon } from "./PaperClipIcon";
 import { SquareIcon } from "./SquareIcon";
 import { TokenIcon } from "./TokenIcon";
 
 const CustomIcons = {
-  ArrowUpIcon,
-  CheckIcon,
-  CreateAgentIcon,
-  FilterIcon,
-  FolderQ1kIcon,
-  PaperClipIcon,
-  SquareIcon,
-  TokenIcon,
+  "ArrowUpIcon":ArrowUpIcon,
+  "CheckIcon":CheckIcon,
+  "CreateAgentIcon":CreateAgentIcon,
+  "FilterIcon":FilterIcon,
+  "FolderQ1kIcon":FolderQ1kIcon,
+  "PaperClipIcon":PaperClipIcon,
+  "SquareIcon":SquareIcon,
+  "TokenIcon":TokenIcon,
+  "GoogleDriveIcon":GoogleDriveIcon,
+  "FileCSVIcon":FileCSVIcon,
+  "FilePDFIcon":FilePDFIcon,
+  "FileDOCIcon":FileDOCIcon
 };
 
 export type CustomIconName = keyof typeof CustomIcons;
