e4cc2d2bf5160020b0e8ada9f36d2ab9710a2c1b
anuj-karan-q1k <anuj@q1k.ai>
2025-12-22 17:00:06 +0530

    feat: dist bump

--- dist/esm/icons/FileDOCIcon.d.ts
+++ dist/esm/icons/FileDOCIcon.d.ts
@@ -1,7 +1,7 @@
 import React from "react";
-export interface FilterIconProps {
+export interface FileDOCIconProps {
     className?: string;
     size?: number;
     strokeWidth?: number;
 }
-export declare const FilterIcon: React.FC<FilterIconProps>;
+export declare const FileDOCIcon: React.FC<FileDOCIconProps>;
---
+++ dist/esm/icons/GoogleDriveIcon.d.ts
@@ -0,0 +1,7 @@
+import React from "react";
+export interface GoogleDriveIconProps {
+    className?: string;
+    size?: number;
+    strokeWidth?: number;
+}
+export declare const GoogleDriveIcon: React.FC<GoogleDriveIconProps>;
