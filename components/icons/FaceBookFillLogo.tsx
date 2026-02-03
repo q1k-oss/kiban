4b52a7ea4e84f786a4d3158e0b49b1ddf2a4d173
anuj-karan-q1k <anuj@q1k.ai>
2026-02-04 01:01:37 +0530

    feat: created custom-icons facebook-fill, linkedin, reddit and x(twitter)

---
+++ components/icons/FaceBookFillLogo.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+export interface IFaceBookFillLogo {
+  size?: number;
+  className?: string;
+  strokeWidth?: number;
+}
+export const FaceBookFillLogo: React.FC<IFaceBookFillLogo> = ({
+  size = 20,
+  className,
+  strokeWidth = 1.5,
+}) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 10 19"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M6.49088 19V10.3336H9.43928L9.88066 6.95612H6.49088V4.79895C6.49088 3.82101 6.76609 3.15464 8.18731 3.15464L10 3.15377V0.133118C9.68668 0.0919583 8.61056 0 7.35876 0C4.74516 0 2.95588 1.57398 2.95588 4.46446V6.95507H0V10.3326H2.95579V18.9991H6.48947L6.49088 19Z"
+        fill="currentColor"
+        strokeWidth={strokeWidth}
+      />
+    </svg>
+  );
+};
